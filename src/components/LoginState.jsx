b81e4369b4d390c75b35aed800cd8a1554a471b0
1-and-ME <nitin.oneand@gmail.com>
2026-02-24 10:08:15 +0530

    section complete made cusotm hook for input

--- src/components/LoginState.jsx
+++ src/components/LoginState.jsx
@@ -1,85 +1,79 @@
-import { useState } from "react";
 import Input from "./Input";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
+import { useInput } from "../hooks/useInput.js";
 
 export default function Login() {
-   // const [enteredEmail, setEnteredEmail] = useState("");
-   // const [enteredPassword, setEnteredPassword] = useState("");
+    // const [enteredEmail, setEnteredEmail] = useState("");
+    // const [enteredPassword, setEnteredPassword] = useState("");
 
-   const [enteredInput, setEnteredInput] = useState({
-      email: "",
-      password: "",
-   });
+    const {
+        value: emailValue,
+        handleInputBlur: handleEmailBlur,
+        handleInputChange: handleEmailChange,
+        hasError: emailHasError,
+    } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
 
-   const [didEdit, setDidEdit] = useState({
-      email: false,
-      password: false,
-   });
+    const {
+        value: passwordValue,
+        handleInputBlur: handlePasswordBlur,
+        handleInputChange: handlePasswordChange,
+        hasError: passwordHasError,
+    } = useInput("", (value) => hasMinLength(value, 6));
 
-   const emailISInvalid = didEdit.email && !enteredInput.email.includes("@");
-   const passwordISInvalid =
-      didEdit.password && enteredInput.password.trim().length < 6;
+    function handleSubmit(e) {
+        e.preventDefault();
 
-   function handleSubmit(e) {
-      e.preventDefault();
-      console.log(enteredInput);
-   }
+        if (emailHasError || passwordHasError) {
+            return;
+        }
 
-   function handleInputChange(value, itendtifer) {
-      setEnteredInput((prev) => ({ ...prev, [itendtifer]: value }));
-   }
+        console.log(enteredInput);
+    }
 
-   function handleInputBlur(itendtifer) {
-      setDidEdit((prev) => ({ ...prev, [itendtifer]: true }));
-   }
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
 
-   return (
-      <form onSubmit={handleSubmit}>
-         <h2>Login</h2>
+            <div className="control-row">
+                <Input
+                    label="email"
+                    id="email"
+                    type="email"
+                    name="email"
+                    onBlur={handleEmailBlur}
+                    onChange={handleEmailChange}
+                    value={emailValue}
+                    error={emailHasError && "Please enter a valid email!"}
+                />
 
-         <div className="control-row">
-            <Input
-               label="email"
-               id="email"
-               type="email"
-               name="email"
-               onBlur={() => handleInputBlur("email")}
-               onChange={(event) =>
-                  handleInputChange("email", event.target.value)
-               }
-               value={enteredInput.email}
-               error={emailISInvalid && "Please enter a valid email!"}
-            />
+                <Input
+                    label="Password"
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={handlePasswordChange}
+                    onBlur={handlePasswordBlur}
+                    value={passwordValue}
+                    error={passwordHasError && "Please enter a valid password!"}
+                />
+            </div>
 
-            <Input
-               label="Password"
-               id="password"
-               type="password"
-               name="password"
-               onChange={(event) =>
-                  handleInputChange("password", event.target.value)
-               }
-               onBlur={() => handleInputBlur("password")}
-               value={enteredInput.password}
-               error={passwordISInvalid && "Please enter a valid password!"}
-            />
-         </div>
+            <p className="form-actions">
+                <button className="button button-flat">Reset</button>
+                {/* defult button type is submit and because of that it relod the page or componenets (reason is html send http request after sending sumbit the form by defult ) */}
+                {/* so for preventing that need to declair type button */}
+                {/* this is one way, but we don't do in this way */}
+                {/* we add even listener to the form  */}
+                <button
+                    className="button"
+                    // type="button"
+                    // onClick={handleSubmit}
+                >
+                    Login
+                </button>
+            </p>
 
-         <p className="form-actions">
-            <button className="button button-flat">Reset</button>
-            {/* defult button type is submit and because of that it relod the page or componenets (reason is html send http request after sending sumbit the form by defult ) */}
-            {/* so for preventing that need to declair type button */}
-            {/* this is one way, but we don't do in this way */}
-            {/* we add even listener to the form  */}
-            <button
-               className="button"
-               // type="button"
-               // onClick={handleSubmit}
-            >
-               Login
-            </button>
-         </p>
-
-         {/* 
+            {/* 
                 <p className="form-actions">
                 <button className="button button-flat">Reset</button>
                  defult button type is submit and because of that it relod the page or componenets (reason is html send http request after sending sumbit the form by defult ) 
@@ -95,6 +89,6 @@ export default function Login() {
                     </button>
                 </p>
             */}
-      </form>
-   );
+        </form>
+    );
 }
